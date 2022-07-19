74940b396aa7e6c1079db21cb08714640d76a113
Daniel Calle <nieled@riseup.net>
2022-07-18 22:18:34 -0500

    implement RabbitMQ withState

--- src/Adapter/RabbitMQ/Common.hs
+++ src/Adapter/RabbitMQ/Common.hs
@@ -13,8 +13,18 @@ withState connectionUri prefetchCount action = bracket initState
                                                        destroyState
                                                        action'
  where
-  initState                = undefined
-  openConnectionAndChannel = undefined
-  destroyState             = undefined
-  action'                  = undefined
-
+  initState = do
+    publisher <- openConnectionAndChannel
+    consumer  <- openConnectionAndChannel
+    return (publisher, consumer)
+  openConnectionAndChannel = do
+    connection <- openConnection'' . fromURI $ connectionUri
+    channel    <- openChannel connection
+    confirmSelect channel False
+    qos channel 0 (fromInteger prefetchCount) True
+    return (connection, channel)
+  destroyState ((connection1, _), (connection2, _)) = do
+    closeConnection connection1
+    closeConnection connection2
+  action' ((_, pubChannel), (_, conChannel)) =
+    action (State pubChannel conChannel)
