185f92e6f5aba4319b7aa762efd046578e8f7cb7
Daniel Calle <nieled@riseup.net>
2022-07-23 14:53:18 -0500

    implement main scotty fns

---
+++ src/Adapter/HTTP/Main.hs
@@ -0,0 +1,59 @@
+module Adapter.HTTP.Main where
+
+import qualified Adapter.HTTP.API.Auth         as AuthAPI
+import           Control.Monad.Reader           ( MonadIO
+                                                , MonadTrans(..)
+                                                )
+import           Data.Text.Lazy                 ( Text )
+import           Domain.Auth                    ( AuthRepo
+                                                , EmailVerificationNotif
+                                                , SessionRepo
+                                                )
+import           Katip                          ( KatipContext
+                                                , Severity(..)
+                                                , logTM
+                                                , ls
+                                                )
+import           Network.HTTP.Types             ( status500 )
+import           Network.Wai                    ( Response )
+import           Network.Wai.Middleware.Gzip    ( GzipFiles(GzipCompress)
+                                                , GzipSettings(gzipFiles)
+                                                , def
+                                                , gzip
+                                                )
+import           Web.Scotty.Trans               ( ScottyError(showError)
+                                                , ScottyT
+                                                , defaultHandler
+                                                , json
+                                                , middleware
+                                                , scottyT
+                                                , status
+                                                )
+
+main
+  :: ( MonadIO m
+     , KatipContext m
+     , AuthRepo m
+     , EmailVerificationNotif m
+     , SessionRepo m
+     )
+  => Int
+  -> (m Response -> IO Response)
+  -> IO ()
+main port runner = scottyT port runner routes
+
+routes
+  :: ( MonadIO m
+     , KatipContext m
+     , AuthRepo m
+     , EmailVerificationNotif m
+     , SessionRepo m
+     )
+  => ScottyT Text m ()
+routes = do
+  middleware $ gzip $ def { gzipFiles = GzipCompress }
+  AuthAPI.routes
+  defaultHandler $ \e -> do
+    lift $ $(logTM) ErrorS $ "Unhandled error: " <> ls (showError e)
+    status status500
+    json ("InternalServerError" :: Text)
