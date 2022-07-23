bb3388f7cbe821ea0b97b72f0551341af5e85a9a
Daniel Calle <nieled@riseup.net>
2022-07-23 14:32:56 -0500

    implement login

--- src/Adapter/HTTP/API/Auth.hs
+++ src/Adapter/HTTP/API/Auth.hs
@@ -2,6 +2,8 @@
 module Adapter.HTTP.API.Auth where
 
 import           Adapter.HTTP.Common            ( parseAndValidateJSON
+                                                , reqCurrentUserId
+                                                , setSessionIdInCookie
                                                 , toResult
                                                 )
 import           Control.Arrow                  ( left )
@@ -11,20 +13,20 @@ import           Control.Monad.Reader           ( MonadIO
 import           Data.Text                      ( Text
                                                 , pack
                                                 )
-import           Domain.Auth                    ( Auth(Auth)
+import           Domain.Auth                    ( Auth(..)
                                                 , AuthRepo
                                                 , EmailValidationErr
-                                                , EmailVerificationError
-                                                  ( EmailVerificationErrorInvalidCode
-                                                  )
+                                                , EmailVerificationError(..)
                                                 , EmailVerificationNotif
-                                                , RegistrationError
-                                                  ( RegistrationErrorEmailTaken
-                                                  )
+                                                , LoginError(..)
+                                                , RegistrationError(..)
                                                 , SessionRepo
                                                 , VerificationCode
+                                                , getUser
+                                                , login
                                                 , mkEmail
                                                 , mkPassword
+                                                , rawEmail
                                                 , register
                                                 , verifyEmail
                                                 )
@@ -32,12 +34,13 @@ import           Katip                          ( KatipContext )
 import           Network.HTTP.Types             ( status400 )
 import qualified Text.Digestive                as DF
 import           Text.Digestive.Form            ( (.:) )
-import           Web.Scotty.Internal.Types      ( ScottyError
+import           Web.Scotty.Internal.Types      ( ScottyError(stringError)
                                                 , ScottyT
                                                 )
 import           Web.Scotty.Trans               ( get
                                                 , json
                                                 , post
+                                                , raise
                                                 , status
                                                 )
 
@@ -67,10 +70,30 @@ routes = do
         status status400
         json ("InvalidCode" :: Text)
       Right _ -> return ()
-  post "/api/auth/login" undefined
-  get "/api/users" undefined
+  post "/api/auth/login" $ do
+    input        <- parseAndValidateJSON authForm
+    domainResult <- lift $ login input
+    case domainResult of
+      Left LoginErrorInvalidAuth -> do
+        status status400
+        json ("InvalidAuth" :: Text)
+      Left LoginErrorEmailNotVerified -> do
+        status status400
+        json ("EmailNotVerified" :: Text)
+      Right sessionId -> do
+        setSessionIdInCookie sessionId
+        return ()
+  get "/api/users" $ do
+    userId <- reqCurrentUserId
+    mEmail <- lift $ getUser userId
+    case mEmail of
+      Nothing ->
+        -- all valid users must have an email
+        raise $ stringError "Should not happen: SessionId map to invalid UserId"
+      Just email -> json $ rawEmail email
 
 -- TODO handle this using specific error types: Domain.Auth.EmailValidationErr Domain.Auth.PasswordValidationErr
+-- | Structure for login and registration endpoints
 authForm :: (Monad m) => DF.Form [Text] m Auth
 authForm = Auth <$> "email" .: emailForm <*> "password" .: passwordForm
  where
