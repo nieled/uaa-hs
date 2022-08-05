32bb6bb1f05cbbf412f6b1898bd009ce55c661cd
Daniel Calle <nieled@riseup.net>
2022-08-04 21:00:29 -0500

    implement users page and verification

--- src/Adapter/HTTP/Web/Auth.hs
+++ src/Adapter/HTTP/Web/Auth.hs
@@ -1,11 +1,18 @@
 module Adapter.HTTP.Web.Auth where
 
-import           Adapter.HTTP.Common
 import           Adapter.HTTP.Web.Common
-import           Control.Monad.Reader           ( MonadIO )
+import           Control.Monad.Reader           ( MonadIO
+                                                , lift
+                                                )
+import           Data.Text                      ( Text )
 import           Domain.Auth
 import           Katip
+import qualified Text.Blaze.Html5              as H
+import           Text.Blaze.Html5               ( (!) )
+import qualified Text.Blaze.Html5.Attributes   as A
+-- import qualified Text.Digestive.Blaze.Html5    as DH
 import           Web.Scotty.Trans
+
 
 routes
   :: ( ScottyError e
@@ -22,9 +29,31 @@ routes = do
   get "/auth/register" $ undefined
   post "/auth/register" $ undefined
 
-  get "/auth/verifyEmail/:code" $ undefined
+  get "/auth/verifyEmail/:code" $ do
+    code   <- param "code" `rescue` const (return "")
+    result <- lift $ verifyEmail code
+    case result of
+      Left EmailVerificationErrorInvalidCode ->
+        renderHtml $ verifyEmailPage "The verification code is invalid"
+      Right _ -> renderHtml $ verifyEmailPage "Your email has been verified"
 
   get "/auth/login" $ undefined
   post "/auth/login" $ undefined
 
-  get "/users" $ undefined
+  get "/users" $ do
+    userId <- reqCurrentUserId
+    mEmail <- lift $ getUser userId
+    case mEmail of
+      Nothing    -> raise $ stringError "Should not happen: email is not found"
+      Just email -> renderHtml $ usersPage (rawEmail email)
+
+usersPage :: Text -> H.Html
+usersPage email = mainLayout "Users" $ do
+  H.div $ H.h1 "Users"
+  H.div $ H.toHtml email
+
+verifyEmailPage :: Text -> H.Html
+verifyEmailPage message = mainLayout "Email verification" $ do
+  H.h1 "Email verification"
+  H.div $ H.toHtml message
+  H.div $ H.a ! A.href "/auth/login" $ "Login"
