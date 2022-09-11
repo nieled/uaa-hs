6cc081d7db7e9c0cce4c2af67d8f0134a619f669
Daniel Calle <nieled@riseup.net>
2022-09-10 23:25:25 -0500

    Define common client types and state fn

--- src/Adapter/HTTP/API/Client/Common.hs
+++ src/Adapter/HTTP/API/Client/Common.hs
@@ -7,10 +7,31 @@ import           Network.HTTP.Client.TLS
 
 type HttpClient r m = (MonadReader r m, Has State r, MonadIO m, MonadThrow m)
 
-newtype Config = Config { configUrl :: String }
+newtype Config
+  = Config { configUrl :: String }
 data State = State
   { stateInitReq :: Request
   , stateManager :: Manager
   }
 
+type Session = CookieJar
 
+data UnexpectedResponse a = UnexpectedResponse Request (Response a)
+  deriving Show
+
+instance (Typeable a, Show a) => Exception (UnexpectedResponse a)
+
+withState :: Config -> (State -> IO a) -> IO a
+withState cfg action = do
+  mgr     <- newManager tlsManagerSettings
+  initReq <- parseRequest $ configUrl cfg
+  let initReqWithJson = initReq
+        { requestHeaders = [("Content-Type", "application/json; charset=utf-8")]
+        }
+  action $ State initReqWithJson mgr
+
+parseOrErr
+  :: (MonadThrow m, FromJSON a) => Request -> Response LByteString -> m a
+parseOrErr req resp = case eitherDecode' $ responseBody resp of
+  Left  _ -> throw $ UnexpectedResponse req resp
+  Right a -> return a
